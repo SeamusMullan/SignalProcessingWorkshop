2166c008ae0d7ad511d19aa1b56a5db3d41e6a0b
Seamus Mullan <43112447+SeamusMullan@users.noreply.github.com>
2025-05-08 11:36:19 +0100

    some modules

---
+++ PythonImplementations/dsp/lpc/lpc.py
@@ -0,0 +1,6 @@
+"""
+Linear Predictive Coding (LPC) is a tool used in speech processing and audio analysis.
+
+Generally used for telecomms and speech stuff, especially in the 80s and 90s for phones and toys (like Speak & Spell).
+
+"""
